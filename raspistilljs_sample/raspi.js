f21ef5291f3b4c2aeed662ef8b78455ecfd0662f
L <t-patoja@microsoft.com>
2017-08-03 11:30:17 -0700

    try http

--- raspistilljs_sample/raspi.js
+++ raspistilljs_sample/raspi.js
@@ -1,9 +1,33 @@
 const Raspistill = require('node-raspistill').Raspistill;
-const raspistill = new Raspistill();
+const raspistill = new Raspistill({
+    width: 640,
+    height: 480,
+    verticalFlip: true,
+    horizontalFlip: true,
+    outputDir: '/home/pi/Desktop'
+});
 
-raspistill.takePhoto()
+function sendPostRequest(url, name) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log(this.responseText);
+    }
+  };
+  xhttp.open("POST", url, true);
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhttp.send("name=" + name);
+}
+
+raspistill.takePhoto('first')
     .then((photo) => {
-        console.log('took photo', photo);
+        console.log('took first photo', photo);
+        this.sendPostRequest("https://ellefuncapp.azurewebsites.net/api/HttpTriggerCSharp1?code=hHOx4SqGNu7scSZWP9njpwY0oKTfXxAL32zLjHntXYePk53qlM5EUQ==",
+         "elle")
+        return raspistill.takePhoto('second');
+    })
+    .then((photo) => {
+        console.log('took second photo', photo);
     })
     .catch((error) => {
         console.error('something bad happened', error);
