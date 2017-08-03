adcd26de527d0d932b118c80d2b7f4e1d7e10b66
L <t-patoja@microsoft.com>
2017-08-03 10:15:14 -0700

    set values to raspividjs

--- raspividjs_sample/raspividjs_sample.js
+++ raspividjs_sample/raspividjs_sample.js
@@ -2,6 +2,10 @@ var raspivid = require('raspivid');
 var fs = require('fs');
  
 var file = fs.createWriteStream(__dirname + '/video.h264');
-var video = raspivid();
+var video = raspivid({
+    rotation: 180,
+    vstab: true,
+    awb: auto
+});
  
 video.pipe(file);
