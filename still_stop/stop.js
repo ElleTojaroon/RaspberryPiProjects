8e308c4cf3eeff381899a54a13687ac808c0db27
L <t-patoja@microsoft.com>
2017-08-03 17:22:49 -0700

    tried stop

---
+++ still_stop/stop.js
@@ -0,0 +1,12 @@
+
+const Raspistill = require('node-raspistill').Raspistill;
+const raspistill = new Raspistill();
+
+raspistill.takePhoto()
+    .then((photo) => {
+        console.log('took photo', photo);
+        raspistill.stop();
+    })
+    .catch((error) => {
+        console.error('something bad happened', error);
+    });
